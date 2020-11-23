34c50a761c886c5068aebd57df2feec5edd53a48
MOTTI Aimé MOTTI <aimemotti@hotmail.fr>
2020-11-23 13:49:13 +0100

    [+] deleting previous div

--- main.js
+++ main.js
@@ -12,28 +12,48 @@ function init() {
 function showInfo(data, tabletop) {
     data.forEach(function (data) {
 
-        testempty.innerHTML = "<h3 id=\"bodyy\"></h3>"
+        /**     HEADER      **/
+        if (!data.img1) {
+            document.getElementById("div1").innerHTML = "<h1 id=\"headertitle\" class=\"banner-header\"></h1>\n" +
+                "        <p id=\"headertext\" class=\"banner-paragraph\"></p>\n" +
+                "        <div id=\"bttn\" class=\"button-box\"></div>";
+        } else {
+            document.getElementById("div1").innerHTML = "<img src=" + data.img1 + " alt=\"\" class=\"logo\"/>\n" +
+                "        <h1 id=\"headertitle\" class=\"banner-header\"></h1>\n" +
+                "        <p id=\"headertext\" class=\"banner-paragraph\"></p>" +
+                "        <div id=\"bttn\" class=\"button-box\"></div>";
+        }
 
-        title.innerHTML = data.title;
-        header.innerHTML = data.header;
-        header2.innerHTML = data.header2;
-        body.innerHTML = data.body;
-        body2.innerHTML = data.body2;
-        body3.innerHTML = data.body3;
-        body4.innerHTML = data.body4;
-        body5.innerHTML = data.body5;
-        body6.innerHTML = data.body6;
-        body7.innerHTML = data.body7;
-        body8.innerHTML = data.body8;
-        body9.innerHTML = data.body9;
-        body10.innerHTML = data.body10;
-        body11.innerHTML = data.body11;
-        body12.innerHTML = data.body12;
-        body13.innerHTML = data.body13;
-        body14.innerHTML = data.body14;
-        body15.innerHTML = data.body15;
-        body16.innerHTML = data.body16;
-        body17.innerHTML = data.body17;
+
+
+        if ((!data.button1 || !data.button1link) && (!data.button2 || !data.button2link)) {
+            document.getElementById("bttn").style.display = "none";
+        } else {
+            document.getElementById("bttn").innerHTML = " <a id=\"button1link\" href=\"#\">\n" +
+                "                <button id=\"button1\" class=\"ios-button\">Copy-edit</button>\n" +
+                "            </a>\n" +
+                "            <a id=\"button2link\" href=\"#\">\n" +
+                "                <button id=\"button2\" class=\"mac-button\">Revise</button>\n" +
+                "            </a>";
+        }
+
+        if (!data.button1 || !data.button1link) {
+            document.getElementById("button1link").style.display = "none";
+            document.getElementById("button1").style.display = "none";
+        } else {
+            document.getElementById("button1link").href = data.button1link;
+            document.getElementById("button1").innerHTML = data.button1;
+        }
+
+        if (!data.button2 || !data.button2link) {
+            document.getElementById("button2link").style.display = "none";
+            document.getElementById("button2").style.display = "none";
+        } else {
+            document.getElementById("button2link").href = data.button2link;
+            document.getElementById("button2").innerHTML = data.button2;
+        }
+
+        /**     SECTION 1       **/
 
         if (/*!data.body || !data.body2 ||
             !data.body3 || !data.body4 ||
@@ -69,20 +89,34 @@ function showInfo(data, tabletop) {
             body18.innerHTML = data.body18;
         }
 
-        if (!data.button1 || !data.button1link) {
-            document.getElementById("button1link").style.display = "none";
-            document.getElementById("button1").style.display = "none";
-        } else {
-            document.getElementById("button1link").href = data.button1link;
-            document.getElementById("button1").innerHTML = data.button1;
-        }
+        icon.href = data.iconimg;
+        icon.type = data.icontype;
+        icon.size = data.iconsize;
+        title.innerHTML = data.title;
+        headertitle.innerHTML = data.headertitle;
+        headertext.innerHTML = data.headertext;
+        body.innerHTML = data.body;
+        body2.innerHTML = data.body2;
+        body3.innerHTML = data.body3;
+        body4.innerHTML = data.body4;
+        body5.innerHTML = data.body5;
+        body6.innerHTML = data.body6;
+        body7.innerHTML = data.body7;
+        body8.innerHTML = data.body8;
+        body9.innerHTML = data.body9;
+        body10.innerHTML = data.body10;
+        body11.innerHTML = data.body11;
+        body12.innerHTML = data.body12;
+        body13.innerHTML = data.body13;
+        body14.innerHTML = data.body14;
+        body15.innerHTML = data.body15;
+        body16.innerHTML = data.body16;
+        body17.innerHTML = data.body17;
 
-        if (!data.button2 || !data.button2link) {
-            document.getElementById("button2link").style.display = "none";
-            document.getElementById("button2").style.display = "none";
-        } else {
-            document.getElementById("button2link").href = data.button2link;
-            document.getElementById("button2").innerHTML = data.button2;
+        /**     FOOTER      **/
+        if (data.autor && data.autorlink) {
+            document.getElementById("footer").innerHTML = data.autor;
+            document.getElementById("footer").href = data.autorlink;
         }
 
     });
